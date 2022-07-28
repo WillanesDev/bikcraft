bea9a0415c6d5530de51e3de4feadd2a59032700
WillanesDev <willanes14@yahoo.com>
2022-07-28 17:20:07 -0300

    adicionado javascript em orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do menu
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link){
@@ -7,5 +8,15 @@ function ativarLink(link){
     link.classList.add('ativo')
   }
 }
-
 links.forEach(ativarLink)
+
+// Ativar itens do orçamento
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro)
+  if(elemento){
+    elemento.checked = true
+  }
+}
+parametros.forEach(ativarProduto)
